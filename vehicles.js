17d180fb581197bd8179f2e6d3570a1c83ec2f25
wanyasmith1111-blip <wanyasmith1111@gmail.com>
2026-05-19 09:03:16 -0400

    Update vehicles.js

--- vehicles.js
+++ vehicles.js
@@ -5,7 +5,7 @@ const vehicles = [
     model: "GT-R",
     year: 2024,
     category: "JDM",
-    image:https://www.edmunds.com/nissan/gt-r/2024/nismo/
+    image: "https://www.edmunds.com/nissan/gt-r/2024/nismo/",
     engine: "3.8L Twin-Turbo V6",
     horsepower: 565,
     drivetrain: "AWD",
@@ -18,7 +18,7 @@ const vehicles = [
     model: "Supra",
     year: 2024,
     category: "JDM",
-    image: https://www.thedrive.com/car-reviews/2024-toyota-gr-supra-review
+    image: "https://www.thedrive.com/car-reviews/2024-toyota-gr-supra-review",
     engine: "3.0L Turbo Inline-6",
     horsepower: 382,
     drivetrain: "RWD",
@@ -31,7 +31,7 @@ const vehicles = [
     model: "M3 Competition",
     year: 2024,
     category: "Euro",
-    image: https://www.google.com/imgres?q=m3%20competition%202024&imgurl=https%3A%2F%2Fi.gaw.to%2Fcontent%2Fphotos%2F55%2F91%2F559131-une-nouvelle-bmw-m3-cs-2024-s-amene-avec-543-chevaux.jpeg%3F1024x640&imgrefurl=https%3A%2F%2Fwww.guideautoweb.com%2Fen%2Farticles%2F69317%2F2024-bmw-m3-cs-delivers-543-horsepower-at-a-staggering-price%2F&docid=WqHO79EmJ5xj9M&tbnid=-HIcpGDaVhPTHM&vet=12ahUKEwjHirn7rsWUAxXs3ckDHbdqOn8QnPAOegQIHBAB..i&w=1024&h=640&hcb=2&ved=2ahUKEwjHirn7rsWUAxXs3ckDHbdqOn8QnPAOegQIHBAB
+    image: "https://www.google.com/imgres?q=m3%20competition%202024&imgurl=https%3A%2F%2Fi.gaw.to%2Fcontent%2Fphotos%2F55%2F91%2F559131-une-nouvelle-bmw-m3-cs-2024-s-amene-avec-543-chevaux.jpeg%3F1024x640&imgrefurl=https%3A%2F%2Fwww.guideautoweb.com%2Fen%2Farticles%2F69317%2F2024-bmw-m3-cs-delivers-543-horsepower-at-a-staggering-price%2F&docid=WqHO79EmJ5xj9M&tbnid=-HIcpGDaVhPTHM&vet=12ahUKEwjHirn7rsWUAxXs3ckDHbdqOn8QnPAOegQIHBAB..i&w=1024&h=640&hcb=2&ved=2ahUKEwjHirn7rsWUAxXs3ckDHbdqOn8QnPAOegQIHBAB",
     engine: "3.0L Twin-Turbo Inline-6",
     horsepower: 503,
     drivetrain: "RWD / AWD",
@@ -44,7 +44,7 @@ const vehicles = [
     model: "Mustang Dark Horse",
     year: 2024,
     category: "Muscle",
-    image: https://www.google.com/imgres?q=2024%20mustang%20dark%20horse&imgurl=https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2F2024-ford-mustang-dark-horse-134-64e76d495cd9d.jpg%3Fcrop%3D0.904xw%3A0.678xh%3B0.0817xw%2C0.202xh%26resize%3D1200%3A*&imgrefurl=https%3A%2F%2Fwww.caranddriver.com%2Freviews%2Fa44675712%2F2024-ford-mustang-dark-horse-drive%2F&docid=9Naqzo1VAr_Q8M&tbnid=DptmsM3JA3KnkM&vet=12ahUKEwiZk_-or8WUAxXl8MkDHfwxPUwQnPAOegQIMxAB..i&w=1200&h=600&hcb=2&ved=2ahUKEwiZk_-or8WUAxXl8MkDHfwxPUwQnPAOegQIMxAB
+    image: "https://www.google.com/imgres?q=2024%20mustang%20dark%20horse&imgurl=https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2F2024-ford-mustang-dark-horse-134-64e76d495cd9d.jpg%3Fcrop%3D0.904xw%3A0.678xh%3B0.0817xw%2C0.202xh%26resize%3D1200%3A*&imgrefurl=https%3A%2F%2Fwww.caranddriver.com%2Freviews%2Fa44675712%2F2024-ford-mustang-dark-horse-drive%2F&docid=9Naqzo1VAr_Q8M&tbnid=DptmsM3JA3KnkM&vet=12ahUKEwiZk_-or8WUAxXl8MkDHfwxPUwQnPAOegQIMxAB..i&w=1200&h=600&hcb=2&ved=2ahUKEwiZk_-or8WUAxXl8MkDHfwxPUwQnPAOegQIMxAB",
     engine: "5.0L V8",
     horsepower: 500,
     drivetrain: "RWD",
